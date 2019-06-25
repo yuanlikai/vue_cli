5c183d1710f3d948a54a041906f91ea216fcea80
袁理开 <1091372693@qq.com>
2019-06-25 14:33:37 +0800

    reset

--- src/router/index.js
+++ src/router/index.js
@@ -24,61 +24,6 @@ export default new Router({
           path: '/product',
           name: 'product',
           component: resolve => require(['@/components/product/product'], resolve)
-        },
-        {
-          path: '/productClass',
-          name: 'productClass',
-          component: resolve => require(['@/components/product/productClass'], resolve)
-        },
-        {
-          path: '/addProduct',
-          name: 'addProduct',
-          component: resolve => require(['@/components/product/addProduct'], resolve)
-        },
-        {
-          path: '/orderList',
-          name: 'orderList',
-          component: resolve => require(['@/components/order/orderList'], resolve)
-        },
-        {
-          path: '/userList',
-          name: 'userList',
-          component: resolve => require(['@/components/user/userList'], resolve)
-        },
-        {
-          path: '/menuList',
-          name: 'menuList',
-          component: resolve => require(['@/components/user/menuList'], resolve)
-        },
-        {
-          path: '/roleList',
-          name: 'roleList',
-          component: resolve => require(['@/components/user/roleList'], resolve)
-        },
-        {
-          path: '/supplierList',
-          name: 'supplierList',
-          component: resolve => require(['@/components/supplier/supplierList'], resolve)
-        },
-        {
-          path: '/addSupplier',
-          name: 'addSupplier',
-          component: resolve => require(['@/components/supplier/addSupplier'], resolve)
-        },
-        {
-          path: '/returnOrder',
-          name: 'returnOrder',
-          component: resolve => require(['@/components/order/returnOrder'], resolve)
-        },
-        {
-          path: '/supplierAccount',
-          name: 'supplierAccount',
-          component: resolve => require(['@/components/supplier/supplierAccount'], resolve)
-        },
-        {
-          path: '/supplierUser',
-          name: 'supplierUser',
-          component: resolve => require(['@/components/supplier/supplierUser'], resolve)
         }
       ]
     }
